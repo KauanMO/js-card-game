cfbd587e7907ce56a4c82d5f1e4ae2165d1a41cb
KauanMO <kauaanmatheus@gmail.com>
2023-08-24 17:49:11 -0300

    WeakAttribute funcionando até segunda ordem

--- js/effects.js
+++ js/effects.js
@@ -9,12 +9,15 @@
             fortifyAnimate(card.querySelector(`#${attribute}Value`))
         }
 
-        function weakAttribute(card, attribute, value) {
-            let cardAttribute = card.querySelector(`#${attribute}Value`).innerText
-            console.log(cardAttribute);
-            if (cardAttribute == 'defense' && Number(cardAttribute) > 1) {
-                cardAttribute = Number(card.querySelector(`#${attribute}Value`).innerText) - value
-                weakAnimate(card.querySelector(`#${attribute}Value`))
+        function weakAttribute(card, attribute, value, classRemove) {
+            let cardAttribute = card.querySelector(`#${attribute}Value`)
+            if (cardAttribute.id == 'defenseValue' && Number(cardAttribute.innerText) < 2) {
+                return
+            }
+            cardAttribute.innerText = Number(card.querySelector(`#${attribute}Value`).innerText) - value
+            weakAnimate(card.querySelector(`#${attribute}Value`))
+            if (classRemove) {
+                card.classList.remove(classRemove)
             }
         }
 
@@ -45,11 +48,13 @@
             let rightSlot = document.querySelector(`.player-slot#slot-${slotId + 1}`)
 
             if (killed) {
-                if(leftSlot.classList.contains('fbDragonTrainer')){
-                    weakAttribute(leftSlot.firstChild, 'strength', 1)
+                if (leftSlot && leftSlot.firstChild && leftSlot.firstChild.classList.contains('fbDragonTrainer')) {
+                    weakAttribute(leftSlot.firstChild, 'strength', 1, 'fbDragonTrainer')
+                    weakAttribute(leftSlot.firstChild, 'defense', 1, 'fbDragonTrainer')
                 }
-                if(rightSlot.classList.contains('fbDragonTrainer')){
-                    weakAttribute(rightSlot.firstChild, 'defense', 1)
+                if (rightSlot && rightSlot.firstChild && rightSlot.firstChild.classList.contains('fbDragonTrainer')) {
+                    weakAttribute(rightSlot.firstChild, 'strength', 1, 'fbDragonTrainer')
+                    weakAttribute(rightSlot.firstChild, 'defense', 1, 'fbDragonTrainer')
                 }
             } else {
                 if (leftSlot && leftSlot.firstChild && verifyDragon(leftSlot.firstChild) && !(leftSlot.firstChild.classList.contains('fbDragonTrainer'))) {
